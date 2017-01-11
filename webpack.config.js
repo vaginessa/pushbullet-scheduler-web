1d2e3c4456c67a2a38f03b00819d7cd5c44a4ae0
전현준 <guswnsxodlf@gmail.com>
2017-01-12 01:49:53 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/**
+ * Created by JHJ on 2017. 1. 11..
+ */
+var path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, '/app/src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        // publicPath: path.join(__dirname, '/app/dist'),
+        path: path.join(__dirname, '/app/dist')
+    },
+    module: {
+        exprContextCritical: false,
+        rules: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['latest', 'react']
+                },
+                test: /\.jsx?$/,
+                include: [
+                    path.join(__dirname, '/app/src')
+                ],
+            },
+            {
+                test: /\.html$/,
+                loader: 'file-loader?name=[name].[ext]',
+                include: [
+                    path.join(__dirname, '/app/src')
+                ],
+            }
+        ]
+    },
+    devtool: "source-map"
+};
