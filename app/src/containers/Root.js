2c343c809e68f3c77da07bd385b239e65401f18f
전현준 <guswnsxodlf@gmail.com>
2017-01-15 00:43:54 +0900

    Add a router and Root component

---
+++ app/src/containers/Root.js
@@ -0,0 +1,21 @@
+/**
+ * Created by JHJ on 2017. 1. 15..
+ */
+"use strict";
+
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+
+export default React.createClass({
+    render(){
+        return (
+            <Menu>
+                <Menu.Item>
+                    awef
+                </Menu.Item>
+            </Menu>
+        );
+    }
+});
+
--- app/src/routes.js
+++ app/src/routes.js
@@ -3,6 +3,29 @@
  */
 'use strict';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import "./index.html"
+import "./index.html";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+
+import Root from './containers/Root.js';
+
+
+const App = React.createClass({
+    render(){
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={Root}>
+                    {/*<IndexRoute component={Home}/>*/}
+                    {/*<Route path="user" component={User}/>*/}
+                    {/*<Route path="login" component={Login}/>*/}
+                </Route>
+            </Router>
+        );
+    }
+});
+
+window.onload = () => {
+    ReactDOM.render(App, document.getElementById("root"));
+};
