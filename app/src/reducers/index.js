03cfb277d20fa4157496856668ae9f830abb3fe0
전현준 <guswnsxodlf@gmail.com>
2017-01-20 23:43:57 +0900

    Add a test action

---
+++ app/src/reducers/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JHJ on 2017. 1. 20..
+ */
+"use strict";
+
+import { combineReducers } from 'redux';
+
+import currentRoute from './currentRoute.js';
+
+
+export default combineReducers({
+    currentRoute
+});
