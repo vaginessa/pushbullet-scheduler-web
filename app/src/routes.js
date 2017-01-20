60a9312413924c347b2e3722f4de1ed5f25f8ada
전현준 <guswnsxodlf@gmail.com>
2017-01-20 23:44:33 +0900

    Wrap router with Provider

--- app/src/routes.js
+++ app/src/routes.js
@@ -10,24 +10,39 @@ import "./scss/main.scss";
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
+import reducers from './reducers';
+
+// Routes
 import Root from './containers/Root.js';
 import Home from './containers/Home.js';
 import List from './containers/List.js';
 
 
+const store = createStore(combineReducers({
+    ...reducers,
+    routing: routerReducer
+}));
+
+const history = syncHistoryWithStore(hashHistory, store);
+
 const App = React.createClass({
     render(){
         return (
-            <Router history={hashHistory}>
-                <Route path="/" component={Root}>
-                    <IndexRoute component={Home}/>
-                    <Route path="list" component={List}/>
-                    {/*<Route path="user" component={User}/>*/}
-                    {/*<Route path="login" component={Login}/>*/}
-                </Route>
-            </Router>
+            <Provider store={store}>
+                <Router history={history}>
+                    <Route path="/" component={Root}>
+                        <IndexRoute component={Home}/>
+                        <Route path="list" component={List}/>
+                        {/*<Route path="user" component={User}/>*/}
+                        {/*<Route path="login" component={Login}/>*/}
+                    </Route>
+                </Router>
+            </Provider>
         );
     }
 });
