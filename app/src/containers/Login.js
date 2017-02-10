de4a64dc83fc661a37450d6dff02178eec41a902
전현준 <guswnsxodlf@gmail.com>
2017-02-11 02:10:55 +0900

    Redirect when login

--- app/src/containers/Login.js
+++ app/src/containers/Login.js
@@ -6,21 +6,29 @@
 import React from 'react';
 import { Container, Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 import LoginPanel from '../components/LoginPanel.js';
 import { fetchAccessToken } from '../actions';
 
 
 const bindStore = (state) => {
-    return state;
+    return {
+        user: state.user
+    };
 };
 
 export default connect(bindStore)(React.createClass({
     loginSubmitClick(value){
         this.props.dispatch(fetchAccessToken(value.id, value.password));
     },
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.user.token !== null) {
+            this.props.dispatch(push('/'));
+        }
+    },
     render(){
-        const loginReducer = this.props.login;
+        const loginReducer = this.props.user;
         return (
             <Container>
                 <Grid centered columns={3} className="stackable">
