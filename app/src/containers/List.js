5b171b14b6bc9c761572b6800d70dd8a1eb16624
전현준 <guswnsxodlf@gmail.com>
2017-02-19 18:53:09 +0900

    Fetch job list data on mount

--- app/src/containers/List.js
+++ app/src/containers/List.js
@@ -5,18 +5,30 @@
 
 import React from 'react';
 import { Container, Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 import JobListPanel from '../components/JobListPanel.js';
+import { fetchJobList } from '../actions/index.js';
 
 
-export default React.createClass({
+const bindStore = (state) => {
+    return {
+        user: state.user,
+        jobList: state.jobList
+    };
+};
+
+export default connect(bindStore)(React.createClass({
+    componentWillMount(){
+        this.props.dispatch(fetchJobList(this.props.user.accessToken));
+    },
     render(){
         return (
             <Container>
                 <Grid stackable={true}>
-                    <JobListPanel/>
+                    <JobListPanel {...this.props.jobList}/>
                 </Grid>
             </Container>
         );
     }
-});
+}));
