ea893b00711a6e082a27a4b16f37dd6e8f4471f7
전현준 <guswnsxodlf@gmail.com>
2017-02-19 14:17:52 +0900

    Update main guide

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -12,8 +12,8 @@ export default React.createClass({
         return (
             <Container>
                 <h1>반갑습니다! Scheduler에 오신 것을 환영합니다.</h1>
-                <h3>Scheduler는 pushbullet과 연동하여 자신이 지정한 시간에 push알림이 오도록 설정할 수 있습니다.</h3>
-                <h5>문의는 guswnsxodlf@gmail.com으로 해주세요!</h5>
+                <h3>Scheduler는 pushbullet과 연동하여 지정한 시간에 push알림이 오도록 설정할 수 있습니다.</h3>
+                <h5>문의, 회원가입은 guswnsxodlf@gmail.com으로 해주세요!</h5>
             </Container>
         );
     }
