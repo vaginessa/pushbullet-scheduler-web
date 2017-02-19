d627d9f08c36cc9158d347532546b274da707c42
전현준 <guswnsxodlf@gmail.com>
2017-02-19 18:53:49 +0900

    Add styles

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -14,15 +14,17 @@ espromise.polyfill();
 // Action types
 export const REQUEST_ACCESS_TOKEN = 'REQUEST_ACCESS_TOKEN';
 export const RECEIVE_ACCESS_TOKEN = 'RECEIVE_ACCESS_TOKEN';
+export const REQUEST_JOB_LIST = 'REQUEST_JOB_LIST';
+export const RECEIVE_JOB_LIST = 'RECEIVE_JOB_LIST';
 
 // Actions creators
-export const requestAccessToken = () => {
+const requestAccessToken = () => {
     return {
         type: REQUEST_ACCESS_TOKEN
     };
 };
 
-export const receiveAccessToken = (data) => {
+const receiveAccessToken = (data) => {
     return {
         type: RECEIVE_ACCESS_TOKEN,
         accessToken: data.accessToken,
@@ -46,7 +48,45 @@ export const fetchAccessToken = (id, password) => {
         return fetch(request).then((res) => {
             if(res.status === 200){
                 res.json().then((data) => {
+                    localStorage.setItem('user', JSON.stringify({
+                        id,
+                        password
+                    }));
                     dispatch(receiveAccessToken(data));
+                });
+            }
+        });
+    };
+};
+
+const requestJobList = () => {
+    return {
+        type: REQUEST_JOB_LIST
+    }
+};
+
+const receiveJobList = (data) => {
+    return {
+        type: RECEIVE_JOB_LIST,
+        count: data.count,
+        rows: data.rows
+    }
+};
+
+export const fetchJobList = (accessToken) => {
+    return (dispatch) => {
+        dispatch(requestJobList());
+        const request = new Request(config.BASE_URL + '/jobs', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+                "accessToken": accessToken
+            }
+        });
+        return fetch(request).then((res) => {
+            if(res.status === 200){
+                res.json().then((data) => {
+                    dispatch(receiveJobList(data));
                 });
             }
         });
