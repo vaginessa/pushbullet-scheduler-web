912c163c15290bcad6ebc24563711a8671a14de9
전현준 <guswnsxodlf@gmail.com>
2017-02-20 16:04:15 +0900

    Update main typo

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -12,8 +12,12 @@ export default React.createClass({
         return (
             <Container>
                 <h1>반갑습니다! Scheduler에 오신 것을 환영합니다.</h1>
-                <h3>Scheduler는 pushbullet과 연동하여 지정한 시간에 push알림이 오도록 설정할 수 있습니다.</h3>
-                <h5>문의, 회원가입은 guswnsxodlf@gmail.com으로 해주세요!</h5>
+                <h3>Scheduler는 Pushbullet과 연동하여 지정한 시간에 push알림이 오도록 설정할 수 있습니다.</h3>
+                <h3>회원가입이나 궁금한 것은 무엇이든지 guswnsxodlf@gmail.com으로 연락해주세요!</h3>
+                <br/>
+                <h1>Welcome to Scheduler!</h1>
+                <h3>Scheduler makes you get the push alarm at any time you want by using Pushbullet.</h3>
+                <h3>Contact me to guswnsxodlf@gmail.com for sign in or anything!</h3>
             </Container>
         );
     }
